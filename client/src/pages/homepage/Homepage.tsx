c99d4855bf1aa56183c292cfb23c59b6301df117
Victoria Fonteny <victoriafonteny@live.fr>
2025-03-24 15:01:57 +0100

    front : homepage

--- client/src/pages/homepage/Homepage.tsx
+++ client/src/pages/homepage/Homepage.tsx
@@ -13,14 +13,17 @@ export default function Homepage() {
   const plants = useLoaderData() as Plant[];
   console.info(plants);
   return (
-    <main>
+    <main className="homepage">
       <header>
         <h1>Plantéo</h1>
         <p>L'atlas du Potager</p>
       </header>
       <section>
-        <h2>Explore 🌱</h2>
-        <p> Découvre les plantes déjà référencés</p>
+        <article className="explore">
+          <h2>Explore 🌱</h2>
+          <p> Découvre les plantes déjà référencés</p>
+          <img className="arrow" src="./arrow.svg" alt="" />
+        </article>
         <ul className="scroll-card-container">
           {plants.map((plant) => (
             <li key={plant.id}>
@@ -34,24 +37,22 @@ export default function Homepage() {
           🌱 Découvrez, cultivez et enrichissez Plantéo en proposant de
           nouvelles plantes pour un potager toujours plus vivant !
         </h2>
-      </section>
-      <section>
-        <h2>
-          🌱 Découvrez, cultivez et enrichissez Plantéo en proposant de
-          nouvelles plantes pour un potager toujours plus vivant !
-        </h2>
-      </section>
-      <section>
-        <h2>
-          🌱 Découvrez, cultivez et enrichissez Plantéo en proposant de
-          nouvelles plantes pour un potager toujours plus vivant !
-        </h2>
-      </section>
-      <section>
-        <h2>
-          🌱 Découvrez, cultivez et enrichissez Plantéo en proposant de
-          nouvelles plantes pour un potager toujours plus vivant !
-        </h2>
+        <ul>
+          <li className="share">
+            <h3>🌱 Partage</h3> Propose tes plantes favorites et enrichis
+            Plantéo
+          </li>
+          <div>
+            <li>
+              <h3>📅 Planifie</h3> Crée des calendriers de semis et de récoltes
+              pour chaque plante
+            </li>
+            <li>
+              <h3>🌿 Apprends</h3> Découvre des astuces et conseils pour
+              cultiver tes plantes avec succès
+            </li>
+          </div>
+        </ul>
       </section>
     </main>
   );
